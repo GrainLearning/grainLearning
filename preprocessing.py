63a5ca3ff9c1f5cdac1391257aef8903a84597ca
Aron <aronpjansen@gmail.com>
2022-04-01 14:33:33 +0200

    Move preprocessing to own file, add possibility of combining pressures and experiment types.

---
+++ preprocessing.py
@@ -0,0 +1,136 @@
+import numpy as np
+import h5py
+
+DATA_PATH = 'data/rnn_data.hdf5'
+PRESSURES = [0.2, 0.5, 1.0]
+EXPERIMENT_TYPES = ['drained', 'undrained']
+
+def prepare_datasets(
+        raw_data: str = DATA_PATH,
+        standardize: bool = True,
+        concatenate_constants: bool = True,
+        pressure: str = '0.2e6',
+        experiment_type: str = 'drained',
+        train_frac: float = 0.7,
+        val_frac: float = 0.15,
+        ):
+
+    datafile = h5py.File(raw_data, 'r')
+
+    inputs, outputs, contacts = _merge_datasets(datafile, pressure, experiment_type)
+
+    if concatenate_constants:
+        inputs = _concatenate_constants(inputs, contacts)
+        data_used = (inputs, outputs)
+    else:
+        data_used = (inputs, outputs, contacts)
+
+    split_data = _make_splits(data_used, train_frac, val_frac)
+
+    if standardize:
+        split_data, train_stats = _standardize_outputs(split_data)
+
+    return split_data, train_stats
+
+def _merge_datasets(datafile, pressure, experiment_type):
+    """
+    Merge the datasets with different pressures and experiment types,
+    if `pressure` or `experiment_type` is 'All'.
+    Otherwise just return the inputs, outputs and contact_params.
+    """
+    if pressure == 'All':
+        pressures = PRESSURES
+    else:
+        # NOTE: relies on pressure being of form x.ye6
+        pressures = [float(pressure[:3])]
+    if experiment_type == 'All':
+        experiment_types = EXPERIMENT_TYPES
+    else:
+        experiment_types = [experiment_type]
+
+    input_sequences = []
+    output_sequences = []
+    contact_params = []
+    for pres in pressures:
+        for exp_type in experiment_types:
+            data = datafile[f'{pres}e6'][exp_type]
+            input_sequences.append(data['inputs'][:])
+            output_sequences.append(data['outputs'][:])
+
+            cps = data['contact_params'][:]
+            cps = _augment_contact_params(
+                    cps, pres, exp_type,
+                    pressure == 'All',
+                    experiment_type == 'All')
+            contact_params.append(cps)
+
+    input_sequences = np.concatenate(input_sequences, axis=0)
+    output_sequences = np.concatenate(output_sequences, axis=0)
+    contact_params = np.concatenate(contact_params, axis=0)
+
+    return input_sequences, output_sequences, contact_params
+
+def _augment_contact_params(
+        contact_params, pressure: float, experiment_type: str,
+        add_pressure: bool, add_type: bool):
+    new_info = []
+    if add_pressure: new_info.append(pressure)
+    if add_type: new_info.append(experiment_type == 'drained')
+
+    new_info = np.expand_dims(new_info, 0)
+    new_info = np.repeat(new_info, contact_params.shape[0], axis=0)
+
+    return np.concatenate([contact_params, new_info], axis=1)
+
+def _concatenate_constants(inputs, contacts):
+    contacts_sequence = np.expand_dims(contacts, axis=1)
+    sequence_length = inputs.shape[1]
+    contacts_sequence = np.repeat(contacts_sequence, sequence_length, 1)
+    total_inputs = np.concatenate([inputs, contacts_sequence], axis=2)
+    return total_inputs
+
+def _make_splits(data, train_frac, val_frac):
+    """
+    Split data into train, val, test sets,  based on samples,
+    not within a sequence.
+    """
+    n_tot = data[0].shape[0]
+    n_train = int(train_frac * n_tot)
+    n_val = int(val_frac * n_tot)
+    split_data = {
+            'train': tuple(d[:n_train] for d in data),
+            'val': tuple(d[n_train:n_train + n_val] for d in data),
+            'test': tuple(d[n_train + n_val:] for d in data),
+            }
+    return split_data
+
+def _standardize_outputs(split_data):
+    """
+    Standardize outputs, using the mean and std of the training data.
+    """
+    # NOTE: relies on outputs being at index 1!
+    y_mean = split_data['train'][1].mean(axis=(0, 1)),
+    train_stats = {
+        'y_mean': y_mean,
+        'y_std': (split_data['train'][1] - y_mean).std(axis=(0, 1)),
+        }
+
+    standardized_splits = dict()
+    for split in ['train', 'val', 'test']:
+        standardized_splits[split] = _standardize(split_data[split], train_stats)
+
+    return standardized_splits, train_stats
+
+def _standardize(data, stats):
+    data = list(data)
+    data[1] = (data[1] - stats['y_mean']) / stats['y_std']
+    data = tuple(data)
+    return data
+
+def main():
+    prepare_datasets(pressure='All', experiment_type='All')
+
+
+if __name__ == '__main__':
+    main()
+
--- simple_rnn.py
+++ simple_rnn.py
@@ -10,71 +10,17 @@ from tensorflow import keras
 from tensorflow.keras import layers, Sequential
 import numpy as np
 from matplotlib import pyplot as plt
-import h5py
 import os
 
-datafile = h5py.File('data/rnn_data.hdf5', 'r')
-pressure = '0.2e6'
-experiment_type = 'drained'
+from preprocessing import prepare_datasets
 
-data = datafile[pressure][experiment_type]
-inputs = data['inputs'][:]
-outputs = data['outputs'][:]
-contact_params = data['contact_params'][:]
-num_samples, sequence_length, num_labels = outputs.shape
+pressure = 'All'  # '0.2e6'
+experiment_type = 'All'  # 'drained'
 
-# concatenate contact parameters to inputs
-contact_params_sequence = np.expand_dims(contact_params, axis=1)
-contact_params_sequence = np.repeat(contact_params_sequence, sequence_length, 1)
-total_inputs = np.concatenate([inputs, contact_params_sequence], axis=2)
-
-num_features = total_inputs.shape[2]
-
-def make_splits(inputs, outputs, fracs):
-    """
-    Split data into train, val, test based on samples (contact params),
-    not within a sequence.
-    """
-    n_tot = inputs.shape[0]
-    n_train = int(fracs['train'] * n_tot)
-    n_val = int(fracs['val'] * n_tot)
-    split_data = {
-            'train': (inputs[:n_train], outputs[:n_train]),
-            'val': (inputs[n_train:n_train + n_val], outputs[n_train:n_train + n_val]),
-            'test': (inputs[n_train + n_val:], outputs[n_train + n_val:])
-            }
-    return split_data
-
-split_fracs = {'train': 0.7, 'val': 0.15, 'test': 0.15}
-split_data = make_splits(total_inputs, outputs, split_fracs)
-
-def standardize_splits(split_data):
-    """
-    Standardize all 3 splits, using the mean and std of the training data.
-    Do this for the input features and the output labels.
-    """
-    X_mean = split_data['train'][0].mean(axis=(0, 1)),
-    y_mean = split_data['train'][1].mean(axis=(0, 1)),
-    train_stats = {
-        'X_mean': X_mean,
-        'X_std': (split_data['train'][0] - X_mean).std(axis=(0, 1)),
-        'y_mean': y_mean,
-        'y_std': (split_data['train'][1] - y_mean).std(axis=(0, 1)),
-        }
-
-    standardized_splits = dict()
-    for split in ['train', 'val', 'test']:
-        standardized_splits[split] = standardize(split_data[split], train_stats)
-
-
-    return train_stats, standardized_splits
-
-def standardize(data, stats):
-    X, y = data
-    return ((X - stats['X_mean']) / stats['X_std'],
-                (y - stats['y_mean']) / stats['y_std'])
-
-train_stats, split_data = standardize_splits(split_data)
+split_data, train_stats = prepare_datasets(
+        pressure=pressure,
+        experiment_type=experiment_type,
+        )
 
 def sliding_windows(data, window_size: int, window_step: int):
     """
@@ -111,17 +57,20 @@ window_size, window_step = 20, 5
 windows = {split: sliding_windows(split_data[split], window_size, window_step)
             for split in ['train', 'val', 'test']}
 
+_, sequence_length, num_features = split_data['train'][0].shape
+num_labels = windows['train'][1].shape[-1]
+
 # this is close to Ma's model (apart from the LSTM modification) 
 model = Sequential([
         layers.Input(shape=(None, num_features)),
         layers.LSTM(50),
-        layers.Dense(20),
+        layers.Dense(20, activation='relu'),
         layers.Dense(num_labels),
         ])
 optimizer = keras.optimizers.Adam(lr=1e-3)
 model.compile(optimizer=optimizer, loss='mse', metrics=['mae'])
 
-epochs = 100  # Ma: 2_000
+epochs = 4  # Ma: 2_000
 batch_size = 256
 history = model.fit(
         *windows['train'],
@@ -171,6 +120,10 @@ test_predictions = predict(test_inputs, model, window_size)
 steps = np.array(list(range(sequence_length)))
 steps_predicted = np.array(list(range(window_size, sequence_length)))
 
+# just to get the plot labels
+import h5py
+datafile = h5py.File('data/rnn_data.hdf5', 'r')
+
 fig, ax = plt.subplots(3, 3)
 for feature_idx in range(test_outputs.shape[2]):
     i = feature_idx % 3
@@ -178,7 +131,6 @@ for feature_idx in range(test_outputs.shape[2]):
     ax[i, j].plot(steps, test_outputs[0, :, feature_idx], label='truth')
     ax[i, j].plot(steps_predicted, test_predictions[0, :, feature_idx], label='predictions')
     ax[i, j].set_title(datafile.attrs['outputs'][feature_idx])
-fig.legend()
-fig.savefig('plots/' + f'test_predictions.png')
+fig.suptitle(f'pressure {pressure}, type {experiment_type}')
+fig.savefig('plots/' + f'test_predictions_{pressure}_{experiment_type}.png')
 
-import IPython ; IPython.embed() ; exit(1)
