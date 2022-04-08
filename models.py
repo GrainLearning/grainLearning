6ce56944f85dd235ddd419ec50cf6e68b5871360
Aron <aronpjansen@gmail.com>
2022-04-08 11:25:28 +0200

    Add e_0 to contact parameters

--- models.py
+++ models.py
@@ -21,3 +21,24 @@ def baseline_model(num_features: int, num_labels: int,
         ])
 
     return model
+
+
+def baseline_model_seq(num_features: int, num_labels: int,
+        lstm_units: int = 50, dense_units: int = 20,
+        seed: int = 42):
+    """
+    Baseline model based on Ma et al., meant to work on data where contact
+    parameters are simply concatenated on to the sequence data.
+    Default parameters also taken from Ma et al.
+    """
+
+    tf.random.set_seed(seed)
+
+    model = Sequential([
+        layers.Input(shape=(None, num_features)),
+        layers.LSTM(lstm_units, return_sequences=True),
+        layers.Dense(dense_units, activation='relu'),
+        layers.Dense(num_labels),
+        ])
+
+    return model
