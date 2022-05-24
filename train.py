54e3bdc836bf7202076b31bc530cfcef13b854aa
Aron <aronpjansen@gmail.com>
2022-05-24 13:44:31 +0200

    Fix wandb not saving model.

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ def train(config=None):
     Train a model and report to weights and biases.
 
     A sweep can be created from the command line using a configuration file,
-    for example `conditional_sweep.yaml`, as:
-        `wandb sweep conditional_sweep.yaml`
+    for example `example_sweep.yaml`, as:
+        `wandb sweep example_sweep.yaml`
     And run with the line shown subsequently in the terminal.
 
     The config is loaded from the yaml file.
@@ -33,41 +33,43 @@ def train(config=None):
                 train_stats['num_contact_params'],
                 train_stats['num_labels'],
                 **config,
-                )
+            )
         optimizer = keras.optimizers.Adam(learning_rate=config.learning_rate)
         model.compile(
                 optimizer=optimizer,
                 loss='mse',
                 metrics=['mae'],
-                )
-
-        # set up training
-        early_stopping = keras.callbacks.EarlyStopping(
-            monitor='val_loss',
-            patience=config.patience,
-            restore_best_weights=True,
-            )
-        wandb_callback = wandb.keras.WandbCallback(
-                monitor='val_root_mean_squared_error',
-                save_model=True,
-                save_weights_only=True,
             )
 
         # create batches
         for split in ['train', 'val', 'test']:
             split_data[split] = split_data[split].batch(config.batch_size)
 
+        # set up training
+        early_stopping = keras.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=config.patience,
+                restore_best_weights=True,
+            )
+        wandb_callback = wandb.keras.WandbCallback(
+                monitor='val_loss',
+                save_model=True,
+                save_weights_only=True,
+                validation_data=split_data['val'],
+            )
+        callbacks = [wandb_callback, early_stopping]
+
         # train
         history = model.fit(
                 split_data['train'],
                 epochs=config.epochs,
                 validation_data=split_data['val'],
-                callbacks=[early_stopping, wandb_callback],
-                )
+                callbacks=callbacks,
+            )
 
         # do some predictions on validation data and save plots to wandb.
-        val_prediction_samples = plot_predictions(split_data, model, train_stats)
-        wandb.log({"predictions": val_prediction_samples})
+#         val_prediction_samples = plot_predictions(split_data, model, train_stats)
+#         wandb.log({"predictions": val_prediction_samples})
 
 
 def main(config):
