e4cb641a485718ddd4b8ef06ee70ea8b5667c836
Aron <aronpjansen@gmail.com>
2022-05-24 09:16:02 +0200

    Remove requirement of use_windows for padding the initial part of the sequence

--- preprocessing.py
+++ preprocessing.py
@@ -55,7 +55,7 @@ def prepare_datasets(
     if add_e0:
         contacts = _add_e0_to_contacts(contacts, inputs)
 
-    if use_windows and pad_length:
+    if pad_length:
         inputs = _pad_initial(inputs, pad_length)
         outputs = _pad_initial(outputs, pad_length)
 
