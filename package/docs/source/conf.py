e620cf897cb7989b5fb8b0bf42c63cf609e2000d
Retiefasuarus <retieflubbe@outlook.com>
2022-08-30 20:33:07 +0200

    modify path of rtd

--- package/docs/source/conf.py
+++ package/docs/source/conf.py
@@ -12,6 +12,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("./package"))
 print(sys.path)
 
 # project information ( you can change this )
