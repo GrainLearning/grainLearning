92b8d58ec93e47e366117b277b59e7229dc4ad3e
Retiefasuarus <retieflubbe@outlook.com>
2022-08-30 20:43:19 +0200

    update rtd config again..

--- package/docs/source/conf.py
+++ package/docs/source/conf.py
@@ -12,8 +12,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
-# sys.path.insert(0, os.path.abspath("./package"))
-sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'package')))
+sys.path.insert(0, os.path.abspath("./package"))
+# sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'package')))
                 
 print(sys.path)
 
